2337195028d10ae85dd1910a7132e6d6bd6b06d8
tungchip2510 <tungchip2510@gmail.com>
2025-12-11 17:49:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
     // ======================================================
     // 1. CẤU HÌNH & KHỞI TẠO BIẾN
     // ======================================================
